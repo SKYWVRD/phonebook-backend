9c1ed2017b3428cbf0bc327ab7354d3245c1cce3
Sean Boonzaier <boonzaiersa@gmail.com>
2024-05-18 10:51:20 +0200

    Add Person schema to main app

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 app.use(express.json());
 app.use(express.static('dist'))
